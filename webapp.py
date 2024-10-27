6ced284b04c12c18e86b2378a33415844d2b5b2f
erkamemer <erkamemer@hotmail.com>
2024-10-27 19:59:35 +0300

    Created webapp.py

---
+++ webapp.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        session['username'] = username
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    if 'username' in session:
+        return render_template('home.html', username=session['username'], theme=session.get('theme', 'light'))
+    return redirect(url_for('login'))
+
+@app.route('/set_theme', methods=['POST'])
+def set_theme():
+    session['theme'] = request.form['theme']
+    return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('theme', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
